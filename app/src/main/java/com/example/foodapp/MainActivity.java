d755b9ba6d160823386154df31dfb575cf3ecacc
haki203 <tho387vm@gmail.com>
2022-11-07 08:40:29 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+//aaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
