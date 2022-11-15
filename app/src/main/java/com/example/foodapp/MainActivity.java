ee5dc3d8940161a2a92fedce416ac502c7cf38b5
ThChien123 <chientran02012003@gmail.com>
2022-11-15 23:58:13 +0700

    chien
    OtpActivity, VerifyOtpActivity đã xử lý xong

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -12,9 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitleToolBar();
     }
     public void register(View view){
         Intent i = new Intent(this,RegisterActivity.class);
         startActivity(i);
+    }
+
+    //hàm này chỉ thể hiện tên activity thôi, ko có liên quan tới chức năng code đâu 😘
+    private void setTitleToolBar(){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Main Activity");
+        }
     }
 }
--- app/src/main/java/com/example/foodapp/VerifyOtpActivity.java
+++ app/src/main/java/com/example/foodapp/VerifyOtpActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodapp.Home.HomeActivity;
@@ -25,51 +26,69 @@ import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
 
-public class OTPActivity extends AppCompatActivity {
+public class VerifyOtpActivity extends AppCompatActivity {
 
-    private static final String TAG = OTPActivity.class.getName();
+    static final String TAG = VerifyOtpActivity.class.getName();
 
-    private EditText edtPhoneNumber_OTPActivity;
-    private Button btnVerifyPhoneNumber_OTPActivity;
-    private FirebaseAuth mAuth_OTPActivity;
+    private EditText edtOtp_VerifyOtpActivity;
+    private Button btnSendOtpCode_VerifyOtpActivity;
+    private TextView tvSendOtpAgain_VerifyOtpActivity;
+
+    private FirebaseAuth mAuth_VerifyOtpActivity;
+
+    private String mPhoneNumber_VerifyOtpActivity;
+    private String mVerifycationId_VerifyOtpActivity;
+
+    private PhoneAuthProvider.ForceResendingToken mForceResendingToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpactivity);
+        setContentView(R.layout.activity_verify_otp);
 
+        getDataIntent();
         setTitleToolBar();
         unitui();
 
-        mAuth_OTPActivity = FirebaseAuth.getInstance();
+        mAuth_VerifyOtpActivity = FirebaseAuth.getInstance();
 
-        btnVerifyPhoneNumber_OTPActivity.setOnClickListener(new View.OnClickListener() {
+        btnSendOtpCode_VerifyOtpActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String strPhoneNumber_OTPActivity = edtPhoneNumber_OTPActivity.getText().toString().trim();
-                onClickVerifyPhoneNumber(strPhoneNumber_OTPActivity);
-                
+                String strOtp_VerifyOtpActivity = edtOtp_VerifyOtpActivity.getText().toString().trim();
+                onClickSendOtpCode(strOtp_VerifyOtpActivity);
+            }
+        });
+        tvSendOtpAgain_VerifyOtpActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSendOtpAgain();
             }
         });
     }
 
+    private void getDataIntent(){
+        mPhoneNumber_VerifyOtpActivity = getIntent().getStringExtra("phone_number");
+        mVerifycationId_VerifyOtpActivity = getIntent().getStringExtra("verifycation_id");
+    }
 
     private void setTitleToolBar(){
         if (getSupportActionBar() != null){
-            getSupportActionBar().setTitle("OTP Activity");
+            getSupportActionBar().setTitle("Verify Otp Activity");
         }
     }
 
-    private void unitui(){
-        edtPhoneNumber_OTPActivity = findViewById(R.id.edt_phone_number_OTPActivity);
-        btnVerifyPhoneNumber_OTPActivity = findViewById(R.id.btn_verify_phone_number_OTPActivity);
-
+    private void onClickSendOtpCode(String strOtp_verifyOtpActivity) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerifycationId_VerifyOtpActivity, strOtp_verifyOtpActivity);
+        signInWithPhoneAuthCredential(credential);
     }
-    private void onClickVerifyPhoneNumber(String strPhoneNumber_otpActivity) {
+    private void onClickSendOtpAgain() {
         PhoneAuthOptions options =
-                PhoneAuthOptions.newBuilder(mAuth_OTPActivity)
-                        .setPhoneNumber(strPhoneNumber_otpActivity)       // Phone number to verify
+                PhoneAuthOptions.newBuilder(mAuth_VerifyOtpActivity)
+                        .setPhoneNumber(mPhoneNumber_VerifyOtpActivity)       // Phone number to verify
                         .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
                         .setActivity(this)                 // Activity (for callback binding)
+                        .setForceResendingToken(mForceResendingToken)
                         .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                             @Override
                             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
@@ -78,24 +97,22 @@ public class OTPActivity extends AppCompatActivity {
 
                             @Override
                             public void onVerificationFailed(@NonNull FirebaseException e) {
-                                Toast.makeText(OTPActivity.this,"Verification Failed", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(VerifyOtpActivity.this,"Verification Failed", Toast.LENGTH_SHORT).show();
                             }
 
                             @Override
                             public void onCodeSent(@NonNull String verifycationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                                 super.onCodeSent(verifycationId, forceResendingToken);
-                                goToVerifyOtpActivity(strPhoneNumber_otpActivity, verifycationId);
+                                mVerifycationId_VerifyOtpActivity = verifycationId;
+                                mForceResendingToken = forceResendingToken;
                             }
                         })          // OnVerificationStateChangedCallbacks
                         .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
-
     }
 
-
-
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        mAuth_OTPActivity.signInWithCredential(credential)
+        mAuth_VerifyOtpActivity.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -112,22 +129,22 @@ public class OTPActivity extends AppCompatActivity {
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
                                 // The verification code entered was invalid
-                                Toast.makeText(OTPActivity.this, "The verification code entered was invalid", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(VerifyOtpActivity.this, "The verification code entered was invalid", Toast.LENGTH_SHORT).show();
                             }
                         }
                     }
                 });
     }
-
     private void goToHomeActivity(String phoneNumber) {
         Intent intent_OTPActivity=new Intent(this, HomeActivity.class);
         intent_OTPActivity.putExtra("phone_number", phoneNumber);
         startActivity(intent_OTPActivity);
     }
-    private void goToVerifyOtpActivity(String strPhoneNumber_otpActivity, String verifycationId) {
-        Intent intent_OTPActivity=new Intent(this, HomeActivity.class);
-        intent_OTPActivity.putExtra("phone_number", strPhoneNumber_otpActivity);
-        intent_OTPActivity.putExtra("verifycation_id", verifycationId);
-        startActivity(intent_OTPActivity);
+
+    private void unitui(){
+        edtOtp_VerifyOtpActivity = findViewById(R.id.edt_phone_number_OTPActivity);
+        btnSendOtpCode_VerifyOtpActivity = findViewById(R.id.btn_verify_phone_number_OTPActivity);
+        tvSendOtpAgain_VerifyOtpActivity = findViewById(R.id.tv_send_otp_again_verifyOtp);
+
     }
 }
