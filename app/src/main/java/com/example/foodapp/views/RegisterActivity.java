25dacbc33668a2cbc65650d35266211258a0bf14
haki203 <tho387vm@gmail.com>
2022-11-18 09:09:39 +0700

    update

--- app/src/main/java/com/example/foodapp/views/RegisterActivity.java
+++ app/src/main/java/com/example/foodapp/views/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodapp.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
